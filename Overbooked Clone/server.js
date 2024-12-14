e4e55b978bbfedd62f34b7538c9999c93a81c2c6
Ammarahmedw <ammarhamza960@gmail.com>
2024-12-15 02:34:05 +0500

    bb

--- Overbooked Clone/server.js
+++
@@ -1,7 +0,0 @@
-const PORT = process.env.PORT || 3000;
-const server = require("./index.js");
-require('dotenv').config();
-
-server.listen(PORT, function(){
-    console.log('listening on port: ' + PORT);
-});
