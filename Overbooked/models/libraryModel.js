dc58f0bf2e1c863e757e8845cfe31be9d8cb53c7
Ammarahmedw <ammarhamza960@gmail.com>
2024-12-16 00:09:13 +0500

    env fixes

--- Overbooked/models/libraryModel.js
+++ Overbooked/models/libraryModel.js
@@ -1,33 +1,37 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
-mongoose.connect("mongodb+srv://wolfie:TA_%24tn%40k2NLSAWi@cluster0.fiadc.mongodb.net/bookshelflyDB?retryWrites=true&w=majority")
+// Use environment variable for MongoDB URI
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('Database connected'))
   .catch(err => console.log('Error connecting to database:', err));
 
-
+// Define the schema
 const librarySchema = new mongoose.Schema({
-    bookName: String,
-    issued: Number,
-    available: Number,
-    total: Number,
-    cover: String,
-    rating: Number
+  bookName: String,
+  issued: Number,
+  available: Number,
+  total: Number,
+  cover: String,
+  rating: Number,
 });
 
-const Library = new mongoose.model("Library", librarySchema);
+// Create the model
+const Library = mongoose.model('Library', librarySchema);
 
-// create new book in the library
-// const newBook = new Library({
-//     bookName: "Diary of a Wimpy Kid: The Long Haul",
-//     issued: 160,
-//     available: 40,
-//     total: 200,
-//     cover: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Diary_of_a_Wimpy_Kid_The_Long_Haul_book_cover.jpg/220px-Diary_of_a_Wimpy_Kid_The_Long_Haul_book_cover.jpg",
-//     rating: 5
-// });
-// newBook.save();
-// console.log("newbook saved");
+// Uncomment to create a new book in the library
+const newBook = new Library({
+    bookName: "Diary of a Wimpy Kid: The Long Haul",
+    issued: 160,
+    available: 40,
+    total: 200,
+    cover: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Diary_of_a_Wimpy_Kid_The_Long_Haul_book_cover.jpg/220px-Diary_of_a_Wimpy_Kid_The_Long_Haul_book_cover.jpg",
+    rating: 5
+});
+newBook.save();
+console.log("New book saved");
 
 module.exports = Library;
